c3fb8e23257507bb370264c615a9a41c2d7bb82d
mulyoved <mulyoved@gmail.com>
2014-12-22 21:59:49 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,159 @@
+/* jshint node:true */
+'use strict';
+// generated on 2014-12-21 using generator-gulp-webapp 0.2.0
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+require('gulp-grunt')(gulp);
+var imageResize = require('gulp-image-resize');
+
+gulp.task('styles', function () {
+  return gulp.src('app/styles/main.scss')
+    .pipe($.plumber())
+    .pipe($.sass({
+      style: 'expanded',
+      precision: 10
+    }))
+    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe(gulp.dest('.tmp/styles'));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('html', ['styles'], function () {
+  var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src(['app/images/**/*', '!app/images/logo/**'])
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true
+    })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe($.size({title: 'images'}));
+});
+
+gulp.task('image-resize', function () {
+  gulp.src('app/images/thumbnails/**')
+    .pipe(imageResize({
+      width : 240,
+      height : 190,
+      crop : true,
+      gravity: 'SouthWest',
+      //imageMagick: true,
+      upscale : false
+    }))
+    .pipe(gulp.dest('dist/images/thumbnails/'));
+});
+
+gulp.task('copy-images', function () {
+  gulp.src(['app/images/*.svg'])
+    .pipe(gulp.dest('dist/images/'))
+    .pipe($.size({title: 'copy-images'}));
+
+  gulp.src(['app/images/logo/*'])
+    .pipe(gulp.dest('dist/images/logo'))
+    .pipe($.size({title: 'copy-images'}));
+
+  return gulp.src(['app/images/icons/*'])
+    .pipe(gulp.dest('dist/images/icons'))
+    .pipe($.size({title: 'copy-images'}));
+});
+
+
+gulp.task('fonts', function () {
+  return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', function () {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html',
+    'node_modules/apache-server-configs/dist/.htaccess'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('connect', ['styles'], function () {
+  var serveStatic = require('serve-static');
+  var serveIndex = require('serve-index');
+  var app = require('connect')()
+    .use(require('connect-livereload')({port: 35729}))
+    .use(serveStatic('.tmp'))
+    .use(serveStatic('app'))
+    // paths to bower_components should be relative to the current file
+    // e.g. in app/index.html you should use ../bower_components
+    .use('/bower_components', serveStatic('bower_components'))
+    .use(serveIndex('app'));
+
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
+});
+
+gulp.task('serve', ['connect', 'watch'], function () {
+  require('opn')('http://localhost:9000');
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['connect'], function () {
+  $.livereload.listen();
+
+  // watch for changes
+  gulp.watch([
+    'app/*.html',
+    '.tmp/styles/**/*.css',
+    'app/scripts/**/*.js',
+    'app/images/**/*'
+  ]).on('change', $.livereload.changed);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+gulp.task('build', ['jshint', 'html', 'images', 'image-resize', 'copy-images', 'fonts', 'extras'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
+
+gulp.task('deploy', function() {
+  runSequence('clean', 'build', 'grunt-deploy');
+});
